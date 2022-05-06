7376f5f597f86b5c111cbf3a3e8c73b4574e87db
Thomas Boel Micheelsen <thomasmicheelsen@gmail.com>
2022-05-06 20:19:57 +0200

    working start chunk border

---
+++ src/main/java/com/javabuckets/onechunk/CommandOneChunk.java
@@ -0,0 +1,29 @@
+package com.javabuckets.onechunk;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class CommandOneChunk implements TabExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Chunk startChunk = player.getWorld().getChunkAt(player.getLocation());
+
+            OneChunk.startOneChunk(startChunk);
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
+}
--- src/main/java/com/javabuckets/onechunk/OneChunk.java
+++ src/main/java/com/javabuckets/onechunk/OneChunk.java
@@ -1,9 +1,15 @@
 package com.javabuckets.onechunk;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.WorldBorder;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public final class OneChunk extends JavaPlugin {
 
@@ -12,14 +18,34 @@ public final class OneChunk extends JavaPlugin {
   private static Objective objective;
   private static Team team;
 
+  public static ArrayList<Player> players = new ArrayList<>();
+  public static ArrayList<Chunk> unlockedChunks = new ArrayList<>();
+  public static WorldBorder worldBorder;
+
   @Override
   public void onEnable() {
-    initializeTaskList();
+    // Plugin startup logic
+    this.getCommand("onechunk").setExecutor(new CommandOneChunk());
   }
 
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+  }
+
+  public static void startOneChunk(Chunk startChunk) {
+    // TODO: Get current progress from save file if exists and load it, else create new
+
+    initializeTaskList();
+
+    unlockedChunks.add(startChunk);
+    players.addAll(Bukkit.getOnlinePlayers());
+
+    worldBorder = startChunk.getWorld().getWorldBorder();
+    Location center = new Location(startChunk.getWorld(), startChunk.getX() << 4, 64, startChunk.getZ() << 4).add(8, 0, 8);
+    center.setY(center.getWorld().getHighestBlockYAt(center) + 1);
+    worldBorder.setCenter(center);
+    worldBorder.setSize(16);
   }
 
   public static void initializeTaskList() {
@@ -28,7 +54,7 @@ public final class OneChunk extends JavaPlugin {
     objective = board.registerNewObjective("task name", "task criteria", "Task Display Name");
     objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-    for (Player player : Bukkit.getOnlinePlayers()) {
+    for (Player player : players) {
       player.setScoreboard(board);
     }
   }
